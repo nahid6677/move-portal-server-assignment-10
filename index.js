aceba5f694193c3cd5843f11adfcaa862191f6ed
nahid6677 <mdrajarr025@gmail.com>
2025-01-20 02:53:54 +0600

    complete

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const app = express();
 require('dotenv').config();
@@ -21,7 +21,48 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // await client.connect();
-    
+    const movieCollection = client.db("MovieDB").collection("movie");
+    const favMovieCollection = client.db("FavoriteMovieDB").collection("movie");
+
+    app.post("/movies", async (req, res) => {
+      const movie = req.body;
+      const result = await movieCollection.insertOne(movie);
+      res.send(result)
+    });
+    app.post("/favorite", async (req, res) => {
+      const favMovie = req.body;
+      const result = await favMovieCollection.insertOne(favMovie);
+      res.send(result)
+    });
+    app.get("/favorite", async (req, res) => {
+      const favMovies = favMovieCollection.find();
+      const result = await favMovies.toArray();
+      res.send(result)
+    });
+    app.delete("/favorite/:id", async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await favMovieCollection.deleteOne(query);
+      res.send(result)
+    })
+    app.get("/movies", async (req, res) => {
+      const movies = movieCollection.find();
+      const result = await movies.toArray();
+      res.send(result)
+    });
+    app.get("/movies/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await movieCollection.findOne(query);
+      res.send(result)
+    });
+
+    app.delete("/movies/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await movieCollection.deleteOne(query);
+      res.send(result);
+    })
 
     // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -34,11 +75,11 @@ run().catch(console.dir);
 
 
 
-app.get("/", (req,res)=>{
-    res.send(`Move portal server is running on port: ${port}`)
+app.get("/", (req, res) => {
+  res.send(`Move portal server is running on port: ${port}`)
 })
-app.listen(port, ()=>{
-    console.log(`Move portal server is running on port: ${port}`)
+app.listen(port, () => {
+  console.log(`Move portal server is running on port: ${port}`)
 })
 
 
